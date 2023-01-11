ceeea31b34e2271673eecb88c811185d7a609103
Neon <105565447+BK-06@users.noreply.github.com>
2023-01-11 19:03:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
-function palindrome(str){
 
+//const input = window.prompt();
+const input = "RaceCar";
+
+const str = input.toLowerCase();
+let palindromeStr = "";
+function palindrome(str) {
+  for (let index = 0; index < str.length; index++) {
+    let currChar = str.at(index);
+    palindromeStr = currChar + palindromeStr;
+    console.log(palindromeStr);
+  }
+  if (palindromeStr === str) {
+    return true;
+  }
+  return false;
 }
+
+document.getElementById("palindrome").textContent = palindrome(str);
+
 module.exports = palindrome
